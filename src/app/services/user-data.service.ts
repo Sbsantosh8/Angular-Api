03067bfaa4e92a4c0f2428285499016036f15e26
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-02-19 11:18:57 +0530

    added provideHttpClient() in app.config.ts

--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -1,11 +1,31 @@
+// import { Injectable } from '@angular/core';
+// import { HttpClient} from '@angular/common/http';
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class UserDataService {
+// url = 'https://jsonplaceholder.typicode.com/users';
+//   constructor(private http:HttpClient) { }
+
+//   user(){
+//     return this.http.get(this.url);
+//   }
+// }
+
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Ensures global availability
 })
 export class UserDataService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/todos/1'; // Replace with your API
 
-  constructor() { }
+  constructor(private http: HttpClient) {} // Ensure HttpClient is injected
 
-  
+  user(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/users`);
+  }
 }
